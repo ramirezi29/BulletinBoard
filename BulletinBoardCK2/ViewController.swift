c442f0d772e9597d91f3d71e14a5e35f537f14aa
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2018-09-24 10:48:33 -0600

    Initial Commit

---
+++ BulletinBoardCK2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BulletinBoardCK2
+//
+//  Created by Ivan Ramirez on 9/24/18.
+//  Copyright © 2018 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
